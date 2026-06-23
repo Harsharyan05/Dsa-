c1e58592971c5a18456bea48d040c9535b27e43d
HARSH ARYAN <ha5802@srmist.edu.in>
2026-06-23 17:38:38 +0530

    Add files via upload

---
+++ Pattern17.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    int i = 1;
+
+    // Upper Pyramid
+    while(i <= n) {
+
+        int space = n - i;
+
+        while(space) {
+            cout << " ";
+            space--;
+        }
+
+        int star = 2 * i - 1;
+
+        while(star) {
+            cout << "*";
+            star--;
+        }
+
+        cout << endl;
+        i++;
+    }
+
+    // Lower Pyramid
+    i = n - 1;
+
+    while(i >= 1) {
+
+        int space = n - i;
+
+        while(space) {
+            cout << " ";
+            space--;
+        }
+
+        int star = 2 * i - 1;
+
+        while(star) {
+            cout << "*";
+            star--;
+        }
+
+        cout << endl;
+        i--;
+    }
+
+    return 0;
+}
