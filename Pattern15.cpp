34bc118350d250116967e673080fda91de82a3d0
HARSH ARYAN <ha5802@srmist.edu.in>
2026-06-23 17:24:16 +0530

    Add files via upload

---
+++ Pattern15.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int i = 1;
+
+    while(i <= n) {
+
+        int space = n - i;
+
+        while(space) {
+            cout << " ";
+            space--;
+        }
+        int star = 2 * i - 1;
+        
+        while(star) {
+            cout << "*";
+            star--;
+        }
+
+        cout << endl;
+        i++;
+    }
+    return 0;
+}
