f949f0e2505692f1ae5cc4271a21f3521705c28d
HARSH ARYAN <ha5802@srmist.edu.in>
2026-06-26 18:01:12 +0530

    Add files via upload

---
+++ for_loop14.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+
+    cin >> a >> b;
+
+    for(int num = a; num <= b; num ++) {
+
+        if(num <= 1)
+            continue;
+
+        bool isPrime = true;
+        
+        for(int i = 2; i * i <= num; i++) {
+
+            if(num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime)
+            cout << num << " ";
+    }
+
+    return 0;
+}
