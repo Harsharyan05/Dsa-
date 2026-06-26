71ce310e6b72416c36f954854566b0282ae5adcd
HARSH ARYAN <ha5802@srmist.edu.in>
2026-06-26 17:53:59 +0530

    Add files via upload

---
+++ for_loop13.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int original = n;
+    int sum = 0;
+
+    while(n != 0) {
+
+        int digit = n % 10;
+        sum = sum + digit * digit * digit;
+
+        n = n / 10;
+    }
+
+    if(sum == original)
+        cout << "Armstrong Number";
+    else
+        cout << "Not an Armstrong Number";
+        
+        return 0;
+}
