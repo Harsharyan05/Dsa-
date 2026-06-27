0f75ff23a4cf406aa5e945f4c192c62e31653028
HARSH ARYAN <ha5802@srmist.edu.in>
2026-06-28 01:03:46 +0530

    Add files via upload

---
+++ switch8.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    
+    int choice, units;
+    double bill = 0;
+
+    cout << "1. Domestic\n";
+    cout << "2. Commmercial\n";
+    cout << "3. Industrail\n";
+
+    cout << "Enter Choice: ";
+    cin >> choice;
+
+    cout << "Enter Units: ";
+    cin >> units;
+
+    switch(choice) {
+        case 1: 
+            bill = units * 6;
+            cout << "Bill = Rs. " << bill;
+            break; 
+
+        case 2:
+            bill = units * 9;
+            cout << "Bill = Rs. " << bill;
+            break;
+
+        case 3:
+            bill = units * 12;
+            cout << "Bill = Rs." << bill;
+            break;    
+
+        default:
+            cout << "Invalid choice";    
+    }
+
+    return 0;
+}
