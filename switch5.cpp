98c271bf77894db9b2a7906e94420c16a38c47df
HARSH ARYAN <ha5802@srmist.edu.in>
2026-06-28 00:12:07 +0530

    Add files via upload

---
+++ switch5.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int choice;
+    float inr ;
+
+    cin >> choice;
+    cin >> inr;
+
+
+    switch(choice) {
+        
+        case 1:
+            cout << inr * 0.012;
+            break;
+
+        case 2:
+            cout << inr * 0.011;
+            break;
+
+        case 3:
+            cout << "Invalid";
+    }
+
+    return 0;
+}
