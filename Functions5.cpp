2774f433cfb695eacc0c1933f43e3a906b264c7b
HARSH ARYAN <ha5802@srmist.edu.in>
2026-06-28 11:58:48 +0530

    Add files via upload

---
+++ Functions5.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int decimal(int n) {
+
+    int ans = 0;
+    int i = 0;
+
+    while(n) {
+        int digit = n % 10;
+
+        if(digit)
+        ans += pow(2,i);
+
+        i++;
+        n /= 10;
+    }
+
+    return ans;
+}
+
+int main() {
+    int n;
+    cin >>n;
+    cout << decimal(n);
+}
