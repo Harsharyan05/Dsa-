3c3e1f6b155a47b781a0ebb272e0af4892b8340d
HARSH ARYAN <ha5802@srmist.edu.in>
2026-06-28 12:09:40 +0530

    Add files via upload

---
+++ Functions7.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int reverseNum(int n) {
+
+    int rev = 0;
+    
+    while(n) {
+
+        rev = rev * 10 + n % 10;
+        n /= 10;
+
+    }
+
+    return rev;
+}
+
+bool palindrome(int n) {
+
+    return n ==reverseNum(n);
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (palindrome(n))
+        cout << "Palindrome";
+
+    else 
+        cout << "Not Palindrome";    
+}
